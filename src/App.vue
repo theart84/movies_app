581a2ecd5ccf8f79eb9b9ace40e69639b71f60f2
theart84 <artshadow74@gmail.com>
2021-01-07 18:36:55 +0500

    Add axios, bootstrap. Tune axios and bootstrap. Create environment.

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,19 @@
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-
+import {mapActions} from "vuex";
 export default {
   name: "App",
   components: {
     HelloWorld
-  }
+  },
+  mounted() {
+    this.fetchMovies();
+  },
+  methods: {
+    ...mapActions('movies', ['fetchMovies']),
+  },
+
 };
 </script>
 
